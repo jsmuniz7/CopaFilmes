f46afc1a65ea1d3ba386e29e358d10f05c36216d
jsmuniz7 <jsmuniz7@outlook.com>
2019-06-24 19:43:21 -0300

    Added the backend boilerplate

---
+++ src/backend/copa-filmes-api/CopaFilmes.Api/Features/Championship/ChampionshipController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CopaFilmes.Api.Features.Championship
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChampionshipController : ControllerBase
+    {
+        [HttpGet]
+        public int GetValues()
+        {
+            return 1;
+        }
+    }
+}
---
+++ src/backend/copa-filmes-api/CopaFilmes.Api/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using CopaFilmes.Application.Core;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace CopaFilmes.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            AddApplicationServices(services);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CopaFilmes API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseMvc();
+        }
+
+        private void AddApplicationServices(IServiceCollection services)
+        {
+            AddSwagger(services);
+            AddMediatr(services);
+        }
+
+        private static void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Copa Filmes API",
+                    Description = "ASP.NET Core Web API to provide functionallity to Copa Filmes Championship",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Jonathan Souza Muniz",
+                        Email = "jonathan-muniz@outlook.com",
+                        Url = "https://github.com/jsmuniz7"
+                    },
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        private static void AddMediatr(IServiceCollection services)
+        {
+            const string applicationAssemblyName = "CopaFilmes.Application";
+            var assembly = AppDomain.CurrentDomain.Load(applicationAssemblyName);
+
+            AssemblyScanner
+                .FindValidatorsInAssembly(assembly)
+                .ForEach(result => services.AddScoped(result.InterfaceType, result.ValidatorType));
+
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(FailFastRequestBehavior<,>));
+
+            services.AddMediatR(assembly);
+        }
+    }
+}
